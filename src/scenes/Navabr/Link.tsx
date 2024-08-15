41cf6697266071a8a4ac9c1152c723663ba8dabf
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-08-15 22:28:58 +0300

    link for navbar

--- src/scenes/Navabr/Link.tsx
+++ src/scenes/Navabr/Link.tsx
@@ -2,14 +2,26 @@ import AnchorLink from "react-anchor-link-smooth-scroll"
 
 type Props = {
     page: string;
+    selectedPage: string;
+    setSelectedPage: (value: string) => void;
 }
 
-function Link({
+const Link = ({
     page,
-}
-    : Props) {
+    selectedPage,
+    setSelectedPage,
+}: Props) => {
+    const lowerCasePage = page.toLowerCase().replace(/ /g, "")
     return (
-        <AnchorLink>
+        <AnchorLink
+            className={`${selectedPage === lowerCasePage} ? "text-primary-500" : "" }
+            transition duration-500 hover:text-primary-300   
+            `}
+            href={`#${lowerCasePage}`}
+            onClick={
+                () => setSelectedPage(lowerCasePage)
+            }
+        >
             {page}
         </AnchorLink>
     )
