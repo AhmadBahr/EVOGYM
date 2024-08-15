f0c52d0425cab0ef063b345b75e71086087b9463
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-08-15 22:32:46 +0300

    passing selected page as enums

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Navbar from "./scenes/Navabr/Index";
 import { useState } from "react";
 
+enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "our classes",
+  ContactUs = "contact us",
+}
+
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("home")
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
   return <div className="app bg-gray-20">
-    <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    <Navbar selectedPage={selectedPage} setSelectedPage={selectedPage}/>
   </div>
 
 }
