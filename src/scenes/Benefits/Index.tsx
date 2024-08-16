4432600e3e95e7a8af0580658d18f13e8765da69
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-08-16 16:31:48 +0300

    benefits done

--- src/scenes/Benefits/Index.tsx
+++ src/scenes/Benefits/Index.tsx
@@ -4,10 +4,12 @@ import {
     AcademicCapIcon,
 }
     from "@heroicons/react/24/solid"
+import BenefitsPageGraphic from "../../assets/BenefitsPageGraphic.png";
 import { BenefitType, SelectedPage } from "../../shared/types";
 import { motion } from "framer-motion";
 import Htext from "../../shared/Htext";
 import Benefit from "./Benefit";
+import ActionButton from "../../shared/ActionButton";
 
 const benefits: Array<BenefitType> = [
     {
@@ -82,6 +84,58 @@ const Benefits = ({ setSelectedPage }: Props) => {
                     ))
                 }
             </motion.div>
+            {/* Graphics and Description */}
+            <div className="mt-16 items-center justify-between gap-20 md:flex">
+                {/* Graphics */}
+                <img
+                    className="mx-auto"
+                    alt="-benefits-page-graphic"
+                    src={BenefitsPageGraphic}
+                />
+                {/* Description */}
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: 50 },
+                        visible: { opacity: 1, x: 0 }
+                    }}
+                >
+                    {/*TITLE*/}
+                    <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
+                        <Htext>
+                            MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                            <span className="text-primary-500">FIT</span>
+                        </Htext>
+                    </div>
+                    {/*Descript*/}
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 }
+                        }}
+                    >
+                        <p className="my-5">
+                            Nascetur aenean massa auctor tincidunt. Iaculis potenti amet</p>
+                        <p className="mb-5">
+                            eu sem integer. Fusce sagittis elit in nec aliquam.</p>
+                    </motion.div>
+                </motion.div>
+                {/*Button*/}
+                <div className="relative mt-16">
+                    <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                        <ActionButton setSelectedPage={setSelectedPage}>
+                            Join Now
+                        </ActionButton>
+                    </div>
+                </div>
+            </div>
         </motion.div>
     </section>
 }
