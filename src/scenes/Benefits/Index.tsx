c47d8ca58e2e4595b1420cde5a6ca07a894b4e10
Ahmad Bahr <ahmadbah.r@outlook.com>
2024-08-16 15:48:29 +0300

    adding mapped props

--- src/scenes/Benefits/Index.tsx
+++ src/scenes/Benefits/Index.tsx
@@ -9,6 +9,24 @@ import { motion } from "framer-motion";
 import { Children } from "react";
 import Htext from "../../shared/Htext";
 
+const benefits = [
+    {
+        icon: <HomeModernIcon className="h-10 w-10 text-primary-500" />,
+        title: "State-of-the-Art Equipment",
+        description: "We provide world class fitness equipment, trainers and classes too."
+    },
+    {
+        icon: <UserGroupIcon className="h-10 w-10 text-primary-500" />,
+        title: "Expert and Pro Trainers",
+        description: "We provide world class fitness equipment, trainers and classes too."
+    },
+    {
+        icon: <AcademicCapIcon className="h-10 w-10 text-primary-500" />,
+        title: "Unparalleled Training Classes",
+        description: "We provide world class fitness equipment, trainers and classes too."
+    }
+]
+
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 };
@@ -28,13 +46,15 @@ const Benefits = ({ setSelectedPage }: Props) => {
                 </p>
             </div>
             <div className="md:flex items-center justify-between gap-8 mt-5">
-                
+                <Benefit title="State-of-the-Art Equipment" icon={<HomeModernIcon className="h-10 w-10 text-primary-500" />} />
+                <Benefit title="Expert and Pro Trainers" icon={<UserGroupIcon className="h-10 w-10 text-primary-500" />} />
+                <Benefit title="Unparalleled Training Classes" icon={<AcademicCapIcon className="h-10 w-10 text-primary-500" />} />
             </div>
         </motion.div>
     </section>
 }
-        </motion.div>
-    </section>
+        </motion.div >
+    </section >
 }
 
 export default Benefits
